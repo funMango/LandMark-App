7d348913b723a30ceae3a3940320e258b9df29c9
funMango <bluemango608@gmail.com>
2023-10-16 21:48:32 +0900

    feat: build LankmarkRow View Json에서 Lankmark타입으로 변환한 데이터를 가져와서 row형식으로 view를 구성한다.

---
+++ LankMark/Views/LandmarkRow.swift
@@ -0,0 +1,40 @@
+//
+//  LandMarkRow.swift
+//  LankMark
+//
+//  Created by 이민호 on 10/16/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+        
+    }
+}
+
+
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }    
+}
+
+//#Preview("Tutle Rock") {
+//    LandmarkRow(landmark: landmarks[0])
+//}
+//
+//#Preview("Salmon") {
+//    LandmarkRow(landmark: landmarks[1])
+//}
